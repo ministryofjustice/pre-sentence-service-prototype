243f1d6d21340c6336e9e57f7adaacc46dc2cf42
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2023-08-02 14:59:54 +0100

    Add unpaid work page
    - other pages section nav now links to unpaid work
    - each page's form posts to itself

--- app/routes/2/routes.js
+++ app/routes/2/routes.js
@@ -3,12 +3,18 @@ module.exports = function (router) {
   var version = "2";
 
   router.get('/' + version + '/verification-report/offender-details', function (req, res) {
-    res.render(version + '/verification-report/offender-details', {
-      'baseincomesourceComplete':req.session.data.baseincomesourceComplete
-    })
-});
+    res.render(version + '/verification-report/offender-details')
+  });
 
-router.post('/' + version + '/verification-report/offender-details', function (req, res) {
-  res.redirect('required-sections')
-})
+  router.post('/' + version + '/verification-report/offender-details', function (req, res) {
+    res.redirect('required-sections')
+  })
+
+  router.get('/' + version + '/verification-report/required-sections', function (req, res) {
+    res.render(version + '/verification-report/required-sections')
+  });
+
+  router.post('/' + version + '/verification-report/required-sections', function (req, res) {
+    res.redirect('unpaid-work')
+  })
 }
