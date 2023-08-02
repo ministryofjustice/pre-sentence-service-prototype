0e1f7e84f945aad8c6aba2fde4db71261fbcfd50
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2023-08-02 15:44:16 +0100

    Set up routing for required sections
    All paths also lead to a check answers page so we can show an end.

--- app/routes/2/routes.js
+++ app/routes/2/routes.js
@@ -15,6 +15,34 @@ module.exports = function (router) {
   });
 
   router.post('/' + version + '/verification-report/required-sections', function (req, res) {
-    res.redirect('unpaid-work')
+    const verificationReportSections = req.session.data['verification-report-sections']
+
+    if (verificationReportSections == 'unpaidWork' || verificationReportSections == 'both'){
+      res.redirect('unpaid-work')
+    }else {
+      res.redirect('electronic-monitoring')
+    }
+  })
+
+  router.get('/' + version + '/verification-report/unpaid-work', function (req, res) {
+    res.render(version + '/verification-report/unpaid-work')
+  });
+
+  router.post('/' + version + '/verification-report/unpaid-work', function (req, res) {
+    const verificationReportSections = req.session.data['verification-report-sections']
+
+    if (verificationReportSections == 'both'){
+      res.redirect('electronic-monitoring')
+    }else {
+      res.redirect('check-answers')
+    }
+  })
+
+  router.get('/' + version + '/verification-report/electronic-monitoring', function (req, res) {
+    res.render(version + '/verification-report/electronic-monitoring')
+  });
+
+  router.post('/' + version + '/verification-report/electronic-monitoring', function (req, res) {
+    res.redirect('check-answers')
   })
 }
