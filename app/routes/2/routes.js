03274e55b8522b9544a91fdc2c508e4965353c3f
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2023-08-02 09:31:04 +0100

    Form on offender details links to required sections page
    As we will likely ask questions on this page, inputs should be saved,
    shown when a user returns to the page and the save and continue button
    should link to the next page in the journey.

--- app/routes/2/routes.js
+++ app/routes/2/routes.js
@@ -1,5 +1,14 @@
 module.exports = function (router) {
 
-  var version = "1";
+  var version = "2";
 
+  router.get('/' + version + '/verification-report/offender-details', function (req, res) {
+    res.render(version + '/verification-report/offender-details', {
+      'baseincomesourceComplete':req.session.data.baseincomesourceComplete
+    })
+});
+
+router.post('/' + version + '/verification-report/offender-details', function (req, res) {
+  res.redirect('required-sections')
+})
 }
