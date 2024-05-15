53e5c701975cbfd77c68e3f6a976003e67eaf2f4
titlescreen <richardcooley@gmail.com>
2024-05-15 13:41:56 +0100

    Remove unused code
    - remove unused pages
    - remove unused code from routing

--- app/routes/15/pre-sentence-report.js
+++ app/routes/15/pre-sentence-report.js
@@ -131,201 +131,21 @@ module.exports = function (router) {
     }
   });
 
-  router.get('/' + version + '/pre-sentence-report/write-sentence-proposal', function (req, res) {
+  router.get('/' + version + '/pre-sentence-report/write-sentencing-proposal', function (req, res) {
     req.session.data.pageAction = 'false'
 
-    res.render(version + '/pre-sentence-report/write-sentence-proposal')
+    res.render(version + '/pre-sentence-report/write-sentencing-proposal')
   });
 
-  router.post('/' + version + '/pre-sentence-report/write-sentence-proposal', function (req, res) {
+  router.post('/' + version + '/pre-sentence-report/write-sentencing-proposal', function (req, res) {
     const pageAction = req.session.data['pageAction']
 
     if (pageAction == 'saveDraft'){
-      res.redirect('write-sentence-proposal')
+      res.redirect('write-sentencing-proposal')
     } else {
       res.redirect('preview-report')
     }
   });
-
-  router.get('/' + version + '/pre-sentence-report/essential-information', function (req, res) {
-    req.session.data.pageAction = 'false'
-
-    const domesticAbuseVerificationList = [
-      'Choose an option',
-      'I have not seen of proof domestic abuse checks',
-      'Police checks or Police National Database laptop'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['domestic-abuse-verification']
-      }
-    })
-
-    const safeguardingVerificationList = [
-      'Choose an option',
-      'Social services checks'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['safeguarding-verification']
-      }
-    })
-
-    const healthConditionsVerificationList = [
-      'Choose an option',
-      'Fit note (Statement of Fitness to Work)',
-      'I have not seen proof of health conditions',
-      'Other',
-      'Prescription',
-      'Self reported'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['health-conditions-verification']
-      }
-    })
-
-    const dependantsCaringVerificationList = [
-      'Choose an option',
-      'Email or letter',
-      'I have not seen proof of dependants/caring responsibilities',
-      'Self reported',
-      'Telephone call'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['dependants-caring-verification']
-      }
-    })
-
-    const employmentVerificationList = [
-      'Choose an option',
-      'Email',
-      'I have not seen proof of employment/working patterns',
-      'Other',
-      'Telephone call',
-      'Wage slip or contract'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['employment-verification']
-      }
-    })
-
-    res.render(version + '/pre-sentence-report/essential-information', {
-      domesticAbuseVerificationList,
-      safeguardingVerificationList,
-      healthConditionsVerificationList,
-      dependantsCaringVerificationList,
-      employmentVerificationList
-    })
-  });
-
-    router.post('/' + version + '/pre-sentence-report/essential-information', function (req, res) {
-    const pageAction = req.session.data['pageAction']
-
-    if (pageAction === 'saveDraft'){
-      res.redirect('essential-information')
-    } else {
-      res.redirect('unpaid-work')
-    }
-  });
-
-
-  router.get('/' + version + '/pre-sentence-report/unpaid-work', function (req, res) {
-    req.session.data.pageAction = 'false'
-
-    const benefitsVerificationList = [
-      'Choose an option',
-      'Confirmation from the Department of Work and Pensions (DWP)',
-      'I have not recorded proof of benefits',
-      'Letter from HM Revenue and Customs',
-      'Letter from Jobcentre Plus',
-      'Letter from the Pension Service',
-      'Other',
-      'Pension advice slip',
-      'Recent bank statement',
-      'Self reported'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['benefits-verification']
-      }
-    })
-
-    res.render(version + '/pre-sentence-report/unpaid-work', {benefitsVerificationList})
-  });
-
-  router.post('/' + version + '/pre-sentence-report/unpaid-work', function (req, res) {
-    const pageAction = req.session.data['pageAction']
-
-    if (pageAction == 'saveDraft'){
-      res.redirect('unpaid-work')
-    } else {
-      res.redirect('electronic-monitoring-for-curfew')
-    }
-  })
-
-  router.get('/' + version + '/pre-sentence-report/electronic-monitoring-for-curfew', function (req, res) {
-    req.session.data.pageAction = 'false'
-
-    const electronicMonitoringCurfewVerificationList = [
-      'Choose an option',
-      'I have not recorded consent',
-      'In person',
-      'Telephone call'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['electronic-monitoring-curfew-verification']
-      }
-    })
-
-    res.render(version + '/pre-sentence-report/electronic-monitoring-for-curfew', {electronicMonitoringCurfewVerificationList})
-  });
-
-  router.post('/' + version + '/pre-sentence-report/electronic-monitoring-for-curfew', function (req, res) {
-    const pageAction = req.session.data['pageAction']
-
-    if (pageAction == 'saveDraft'){
-      res.redirect('electronic-monitoring-for-curfew')
-    } else {
-      res.redirect('suitability-decision')
-    }
-  })
-
-  router.get('/' + version + '/pre-sentence-report/suitability-decision', function (req, res) {
-    req.session.data.pageAction = 'false'
-
-    res.render(version + '/pre-sentence-report/suitability-decision')
-  });
-
-  router.post('/' + version + '/pre-sentence-report/suitability-decision', function (req, res) {
-    const pageAction = req.session.data['pageAction']
-    const domesticAbuseResult = req.session.data['domestic-abuse-check-result']
-    const safeguardingResult = req.session.data['safeguarding-check-result']
-    const informedConsent = req.session.data['informed-consent-electronic-monitoring']
-    const suitabilityDecision = req.session.data['suitability-decision']
-
-    if (pageAction == 'saveDraft'){
-      res.redirect('suitability-decision')
-    } else if (suitabilityDecision == 'Electronic monitoring curfew only' || suitabilityDecision == 'Both unpaid work and electronic monitoring curfew') {
-      if (domesticAbuseResult != 'No concerns about domestic abuse' || safeguardingResult != 'No concerns about child safeguarding' || informedConsent != 'Yes, they have given informed consent'){
-        res.redirect('suitability-decision-error')
-      } else {
-        res.redirect('preview-report')
-      }
-    } else {
-      res.redirect('preview-report')
-    }
-  })
 
   router.get('/' + version + '/pre-sentence-report/preview-report', function (req, res) {
     req.session.data.pageAction = 'false'
