18ba1830d91cee1e7aba6c3b45aedc5f2d2ee869
titlescreen <richardcooley@gmail.com>
2024-09-24 08:26:37 +0100

    Update to risk page content
    Review with Faye, Patrick Dawn, Rich. Change to
    questions and order so that it works with the 4
    step process.
    
    - update content
    - remove risk drop down that is not needed
    - update routes file

--- app/routes/18/pre-sentence-report.js
+++ app/routes/18/pre-sentence-report.js
@@ -124,98 +124,6 @@ module.exports = function (router) {
       }
     })
 
-    const riskSeriousHarmToIntimatePartnersList = [
-      'Choose an option',
-      'Low risk',
-      'Medium risk',
-      'High risk',
-      'Very high risk'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['risk-serious-harm-to-intimate-partners']
-      }
-    })
-
-    const riskSeriousHarmToStaffList = [
-      'Choose an option',
-      'Low risk',
-      'Medium risk',
-      'High risk',
-      'Very high risk'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['risk-serious-harm-to-intimate-partners']
-      }
-    })
-
-    const riskSeriousHarmToKnownIndividualsList = [
-      'Choose an option',
-      'Low risk',
-      'Medium risk',
-      'High risk',
-      'Very high risk'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['risk-serious-harm-to-known-individuals']
-      }
-    })
-
-    const riskSeriousHarmToChildrenList = [
-      'Choose an option',
-      'Low risk',
-      'Medium risk',
-      'High risk',
-      'Very high risk'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['risk-serious-harm-to-children']
-      }
-    })
-
-    res.render(version + '/pre-sentence-report/risk-analysis', {
-      riskSeriousHarmToPublicList,
-      riskSeriousHarmToIntimatePartnersList,
-      riskSeriousHarmToStaffList,
-      riskSeriousHarmToKnownIndividualsList,
-      riskSeriousHarmToChildrenList
-    })
-  });
-
-  router.post('/' + version + '/pre-sentence-report/risk-analysis', function (req, res) {
-    const pageAction = req.session.data['pageAction']
-
-    if (pageAction == 'saveDraft'){
-      res.redirect('risk-analysis')
-    } else {
-      res.redirect('sentencing-proposal')
-    }
-  });
-
-  router.get('/' + version + '/pre-sentence-report/risk-analysis-alt', function (req, res) {
-    req.session.data.pageAction = 'false'
-
-    const riskSeriousHarmToPublicList = [
-      'Choose an option',
-      'Low risk',
-      'Medium risk',
-      'High risk',
-      'Very high risk'
-    ].map(item => {
-      return {
-        value: item,
-        text: item,
-        selected: item === req.session.data['risk-serious-harm-to-public']
-      }
-    })
-
     const riskSeriousHarmToStaffList = [
       'Choose an option',
       'Low risk',
@@ -258,7 +166,7 @@ module.exports = function (router) {
       }
     })
 
-    res.render(version + '/pre-sentence-report/risk-analysis-alt', {
+    res.render(version + '/pre-sentence-report/risk-analysis', {
       riskSeriousHarmToPublicList,
       riskSeriousHarmToStaffList,
       riskSeriousHarmToKnownAdultsList,
@@ -266,11 +174,11 @@ module.exports = function (router) {
     })
   });
 
-  router.post('/' + version + '/pre-sentence-report/risk-analysis-alt', function (req, res) {
+  router.post('/' + version + '/pre-sentence-report/risk-analysis', function (req, res) {
     const pageAction = req.session.data['pageAction']
 
     if (pageAction == 'saveDraft'){
-      res.redirect('risk-analysis-alt')
+      res.redirect('risk-analysis')
     } else {
       res.redirect('sentencing-proposal')
     }
